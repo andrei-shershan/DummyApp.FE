efd6dffcd8b106ef06e92b6f995a24aa9ce3495f
Andrei Shershan <mail.shershan@gmail.com>
2026-08-25 01:02:25 +0200

    Avatar upload

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,4 +1,4 @@
-import { UpdateCurrentUserProfileRequest, UserProfile } from '../types/api';
+import { UpdateCurrentUserProfileRequest, UploadCurrentUserAvatarRequest, UserProfile } from '../types/api';
 import { fetchClient } from './fetchClient';
 
 export async function getCurrentUserProfile(): Promise<UserProfile> {
@@ -7,6 +7,13 @@ export async function getCurrentUserProfile(): Promise<UserProfile> {
 
 export async function updateCurrentUserProfile(data: UpdateCurrentUserProfileRequest): Promise<UserProfile> {
   return fetchClient<UserProfile>('/api/users/me', {
+    method: 'PUT',
+    data,
+  });
+}
+
+export async function uploadCurrentUserAvatar(data: UploadCurrentUserAvatarRequest): Promise<UserProfile> {
+  return fetchClient<UserProfile>('/api/users/me/avatar', {
     method: 'PUT',
     data,
   });
